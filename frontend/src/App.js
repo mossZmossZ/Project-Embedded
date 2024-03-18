9e0ac9a2ee03630ec34bd14c2d0f2b605300b730
Nattavee Narischat <nattavee123vee@gmail.com>
2024-03-18 21:56:32 +0700

    Frontend edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Comnarin.jpeg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome Com narin to Frontend Developer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/comnarin/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
         </a>
       </header>
     </div>
