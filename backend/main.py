5e96e53546cd3c586fbb5e9050345790a7100dae
Nattavee Narischat <50925464+mossZmossZ@users.noreply.github.com>
2024-03-23 16:43:09 +0700

    get rfid and query

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 #uvicorn main:app --reload
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,6 +20,26 @@ app.add_middleware(
 
 
 
-@app.get("/api/HelloCom")
+@app.get("/api/")
 async def root():
-    return {"message": "Hello Comnarin"}
+    return {"message": "Hello World"}
+
+
+class RFID(BaseModel):
+    rfid_id: str
+
+@app.post('/api/SentRFID')
+async def read_rfid(request: RFID):
+    data_RFID = {'rfid': request.rfid_id}
+    rfid_id = data_RFID['rfid']  
+    print("RFID ID:", rfid_id)  
+    return {"Susscessful to sent Data"}
+
+@app.get("/api/GetTypeRFID")
+async def TypeRfid(rfid:int):
+    #rfid in database
+    if (rfid ==0) :
+        return {"yes"}
+    else:
+        #rfid not in database
+        return {"Plese Register Member"}
