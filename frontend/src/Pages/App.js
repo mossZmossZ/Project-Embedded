ada83bfca4afee94da14f9b086f869fb2b379b93
Nattavee Narischat <50925464+mossZmossZ@users.noreply.github.com>
2024-03-27 02:16:37 +0700

    eslint no-use

--- frontend/src/Pages/App.js
+++ frontend/src/Pages/App.js
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: 0 */
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
--- frontend/src/Pages/Students.js
+++ frontend/src/Pages/Students.js
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: 0 */
 import React from "react";
 import Sidebar from "../Components/Sidebar";
 import {useState,useEffect} from "react";
